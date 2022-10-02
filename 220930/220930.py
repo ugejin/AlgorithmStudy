4188ea2b14bae51db6a2965c042f9b7e015400e2
ugejin <juj0636@naver.com>
2022-10-02 23:37:33 +0900

    a
    a

---
+++ 220930/220930.py
@@ -0,0 +1,5 @@
+def solution(s):
+    if s.upper().count('P') == s.upper().count('Y'):
+        return True
+    else:
+        return False
---
+++ 221001/221001.py
@@ -0,0 +1,6 @@
+def solution(n):
+    a = []
+    s = reversed(str(n))
+    for i in s:
+        a.append(int(i))
+    return a
